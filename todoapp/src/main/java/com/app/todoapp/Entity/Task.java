4ac30cef66d06dcf8d0915a486d47b16105ecf5b
DhyeyBaldha <dhyeybaldha90@gmail.com>
2025-03-05 21:29:10 +0530

    creating entity and repository

--- todoapp/src/main/java/com/app/todoapp/Entity/Task.java
+++ todoapp/src/main/java/com/app/todoapp/Entity/Task.java
@@ -1,13 +1,14 @@
 package com.app.todoapp.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "Todo-Tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private Boolean completed;
